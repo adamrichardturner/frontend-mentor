013941994f9a87b79977fca461cc023a9315964f
Adam Turner <hello@adamrichardturner.dev>
2024-01-14 06:30:17 +0000

    Cleanup

--- challenges/notifications-page/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const FeedDynamic = dynamic(() => import('@/components/Feed'), { ssr: false })
-
-
-export default function Home() {
-  
-  return <FeedDynamic />
-}
